6b08965bfec4e84d78c8c26aced381b646d19a7b
Martin Mueller <damadden88@googlemail.com>
2023-04-16 19:23:37 +0100

    init

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,25 @@
-import { typescript } from 'projen';
+import { typescript } from "projen";
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 't3-app-projen',
+  defaultReleaseBranch: "main",
+  name: "t3-app-projen",
   projenrcTs: true,
 
+  prettier: true,
+
+  gitignore: [
+    "/.pnp",
+    ".pnp.js",
+    "/prisma/db.sqlite",
+    "/prisma/db.sqlite-journal",
+    "/.next/",
+    "/out/",
+    // "next-env.d.ts",
+    ".DS_Store",
+    "*.pem",
+    ".env",
+    ".env*.local",
+    ".vercel",
+  ],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
